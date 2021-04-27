a0d4297ea17e434ec41ebafb1914f1c420e2321a
Juha Luukkonen <32368550+JuhaLuukkonen@users.noreply.github.com>
2021-04-27 13:04:27 +0300

    Add files via upload
    adding some props input definitions

---
+++ ButtonsResult.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default ({ data, reset, setValue }) => {
+  return (
+    <>
+      {data && (
+        <pre style={{ textAlign: "left", color: "black" }}>
+          {JSON.stringify(
+            {
+              ...data, 
+              Firstname: '', 
+              Lastname: '', 
+              Jobtitle: '',
+              Age:'', 
+              Availability: ''             
+                .text
+            },
+            null,
+            2
+          )}
+        </pre>
+      )}
+
+      <button
+        className="button buttonBlack"
+        type="button"
+        onClick={() => {
+          reset({
+            TextField: "",
+            ReactSelect: { value: "available", label: "Available" },
+            numberFormat: 123456789,   
+          });
+        }}
+      >
+        Reset Form
+      </button>
+      <button className="button">submit</button>
+    </>
+  );
+};
